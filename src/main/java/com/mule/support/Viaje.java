c8a7fdbf41da00aaf3af49eaf2111fce071ebe6f
eduardoxappia <eduardo.rodriguez@xappia.com>
2018-12-18 14:55:36 -0300

    primer commit

---
+++ src/main/java/com/mule/support/Viaje.java
@@ -0,0 +1,45 @@
+package com.mule.support;
+
+public class Viaje {
+
+    private String destino;
+    private String agencia;
+    private Integer costo;
+    private Integer id; // Must be unique per book
+    
+    public Viaje() {
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public Integer getCosto() {
+        return costo;
+    }
+
+    public void setCosto(Integer costo) {
+        this.costo = costo;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+}
